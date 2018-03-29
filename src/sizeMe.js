ea869b428c0211cba53e5d96df8da203167e75dd
joeyJsonar <33907626+joeyJsonar@users.noreply.github.com>
2018-03-29 02:01:16 -0700

    Import fix for lodash.throttle and lodash.debounce (#133)

--- src/sizeMe.js
+++ src/sizeMe.js
@@ -5,8 +5,8 @@ import React, { Children, Component } from 'react'
 import PropTypes from 'prop-types'
 import ReactDOM from 'react-dom'
 import invariant from 'invariant'
-import throttle from 'lodash/throttle'
-import debounce from 'lodash/debounce'
+import throttle from 'lodash.throttle'
+import debounce from 'lodash.debounce'
 import resizeDetector from './resizeDetector'
 
 const defaultConfig = {
