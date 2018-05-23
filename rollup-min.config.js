b6ff655cea9ef1a7a4d0205157e5c38481795ca5
Sean Matheson <sean@ctrlplusb.com>
2018-05-23 10:20:22 +0100

    Updates deps.

--- rollup-min.config.js
+++ rollup-min.config.js
@@ -1,4 +1,4 @@
-const uglify = require('rollup-plugin-uglify')
+const { uglify } = require('rollup-plugin-uglify')
 const packageJson = require('./package.json')
 
 const baseConfig = require('./rollup.config.js')
