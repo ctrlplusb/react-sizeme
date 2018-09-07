470731ae29c9b9e29b751cb5c324ea81ac9b529e
Tiago Serafim <tserafim@gmail.com>
2018-09-07 17:20:46 -0300

    Improved typings declarations.

--- react-sizeme.d.ts
+++ react-sizeme.d.ts
@@ -1,27 +1,25 @@
-declare module 'react-sizeme' {
+import { Component, ComponentType } from 'react'
 
-  type Omit<T, K> = Pick<T, Exclude<keyof T, K>>;
+type Omit<T, K> = Pick<T, Exclude<keyof T, K>>;
 
-  interface SizeMeProps {
-    size: {
-      width: number | null;
-      height: number | null;
-    };
-  }
-
-  export interface SizeMeOptions {
-    monitorWidth?: boolean;
-    monitorHeight?: boolean;
-    monitorPosition?: boolean;
-    refreshRate?: number;
-    refreshMode?: 'throttle' | 'debounce';
-    noPlaceholder?: boolean;
-    children(props: SizeMeProps): JSX.Element;
-  }
-
-  export class SizeMe extends React.Component<SizeMeOptions> {
-  }
-
-  export const withSize: (options?: SizeMeOptions) => <P extends SizeMeProps>(component: React.ComponentType<P>) => React.ComponentType<Omit<P, 'size'>>;
-
+interface SizeMeProps {
+  size: {
+    width: number | null;
+    height: number | null;
+  };
 }
+
+export interface SizeMeOptions {
+  monitorWidth?: boolean;
+  monitorHeight?: boolean;
+  monitorPosition?: boolean;
+  refreshRate?: number;
+  refreshMode?: 'throttle' | 'debounce';
+  noPlaceholder?: boolean;
+  children(props: SizeMeProps): JSX.Element;
+}
+
+export class SizeMe extends Component<SizeMeOptions> {
+}
+
+export const withSize: (options?: SizeMeOptions) => <P extends SizeMeProps>(component: ComponentType<P>) => ComponentType<Omit<P, 'size'>>;
