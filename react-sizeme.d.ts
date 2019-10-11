62e9c91cf7ea3d6bf74ad566f703a78eb1109607
NNNaix <NNNaix@163.com>
2019-10-11 05:28:39 -0500

    Update react-sizeme.d.ts (#171)

--- react-sizeme.d.ts
+++ react-sizeme.d.ts
@@ -2,32 +2,40 @@
 
 import { Component, ComponentType, ReactNode, ReactElement } from 'react'
 
-type Omit<T, K> = Pick<T, Exclude<keyof T, K>>
+declare namespace sizeMe {
+  type Omit<T, K> = Pick<T, Exclude<keyof T, K>>
 
-export interface SizeMeProps {
-  size: {
-    width: number | null
-    height: number | null
+  export interface SizeMeProps {
+    size: {
+      width: number | null
+      height: number | null
+    }
   }
+
+  export interface SizeMeOptions {
+    monitorWidth?: boolean
+    monitorHeight?: boolean
+    monitorPosition?: boolean
+    refreshRate?: number
+    refreshMode?: 'throttle' | 'debounce'
+    noPlaceholder?: boolean
+  }
+
+  export interface SizeMeRenderProps extends SizeMeOptions {
+    children: (props: SizeMeProps) => ReactElement
+  }
+  export class SizeMe extends Component<SizeMeRenderProps> {}
+
+  export const withSize: (
+    options?: SizeMeOptions,
+  ) => <P extends object = {}>(
+    component: ComponentType<P>,
+  ) => ComponentType<Omit<P, 'size'>>
 }
-
-export interface SizeMeOptions {
-  monitorWidth?: boolean
-  monitorHeight?: boolean
-  monitorPosition?: boolean
-  refreshRate?: number
-  refreshMode?: 'throttle' | 'debounce'
-  noPlaceholder?: boolean
-}
-
-export interface SizeMeRenderProps extends SizeMeOptions {
-  children: (props: SizeMeProps) => ReactElement
-}
-
-export class SizeMe extends Component<SizeMeRenderProps> {}
-
-export const withSize: (
-  options?: SizeMeOptions,
-) => <P extends object = {}>(
+declare function sizeMe(
+  options?: sizeMe.SizeMeOptions,
+): <P extends object = {}>(
   component: ComponentType<P>,
-) => ComponentType<Omit<P, 'size'>>
+) => ComponentType<sizeMe.Omit<P, 'size'>>
+
+export = sizeMe
