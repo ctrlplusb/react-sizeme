94ef51fe39e0ec6a26df52dbfaa5c101c7800bde
Sean Matheson <sean@ctrlplusb.com>
2020-01-14 12:26:42 +0000

    Removes old example

--- examples/web/server.js
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-import path from 'path'
-
-const port = process.env.PORT || 1337
-const app = express()
-
-app.use(express.static(path.resolve('./build')))
-
-app.listen(port, () => console.log(`Example running on port ${port}...`))
